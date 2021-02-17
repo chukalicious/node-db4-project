6324f9cdcbb352d3a837189ff7c877807d78da6d
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-02-16 20:40:52 -0800

    getting all recipes

--- api/recipes-router.js
+++ api/recipes-router.js
@@ -4,7 +4,14 @@ const router = express.Router();
 
 const Recipes = require("./recipes-model");
 
-// router.get(){}
+router.get("/", async (req, res) => {
+  try {
+    const recipes = await Recipes.get();
+    res.status(200).json(recipes);
+  } catch (err) {
+    res.status(500).json({ message: "server error" });
+  }
+});
 
 // router.post(){}
 
