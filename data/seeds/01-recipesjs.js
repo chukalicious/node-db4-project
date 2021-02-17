fd18b3789636a5a69cd84d822f513e33fde8dbf5
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-02-16 20:17:38 -0800

    ran seeds for recipe table

---
+++ data/seeds/01-recipesjs.js
@@ -0,0 +1,13 @@
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex("recipes")
+    .del()
+    .then(function () {
+      // Inserts seed entries
+      return knex("recipes").insert([
+        { id: 1, recipe_title: "Cheesecake" },
+        { id: 2, recipe_title: "Pork Roast" },
+        { id: 3, recipe_title: "Lasagna" },
+      ]);
+    });
+};
